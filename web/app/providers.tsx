47fdf414975851005deccc123b9130453abb0832
n4n0byt3 <shivbo1@hotmail.com>
2026-06-12 16:17:01 +0100

    skeleton loading spinner

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -1,8 +1,16 @@
 'use client'
 
+import { useEffect } from 'react'
 import { ThemeProvider } from 'next-themes'
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const el = document.getElementById('init-loader')
+    if (!el) return
+    el.style.opacity = '0'
+    el.addEventListener('transitionend', () => el.remove(), { once: true })
+  }, [])
+
   return (
     <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
       {children}
